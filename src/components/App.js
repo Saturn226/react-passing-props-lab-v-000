c72c691dbc6370a66b8e2376bca2034fecfa57c7
github username <github email address>
2017-09-11 18:26:39 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import FruitBasket from './FruitBasket'
+import 'isomorphic-fetch'
 
-import FruitBasket from './FruitBasket';
+export default class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            fruit: [],
+            filters: [],
+            currentFilter: null
+        }
+    }
 
-const App = () => <FruitBasket />;
+    componentWillMount() {
+        this.fetchFilters();
+        this.fetchFruit()
+      }
 
-export default App;
+      fetchFruit = ( ) => {
+        fetch('/api/fruit')
+        .then(response => response.json())
+        .then(fruit => this.setState({ fruit }));
+      }
+    
+      fetchFilters = () => {
+        fetch('/api/fruit_types')
+          .then(response => response.json())
+          .then(filters => this.setState({ filters }));
+      }
+
+      handleFilterChange = event => {
+        console.log('new filter: ', event.target.value);
+        this.setState({ currentFilter: event.target.value });
+      }
+    
+    render(){
+        return(
+            <FruitBasket 
+            updateFilterCallback={this.handleFilterChange}
+            filters={this.state.filters} 
+            fruit={this.state.fruit} 
+            currentFilter={this.state.currentFilter}/>
+        )
+    }
+}
